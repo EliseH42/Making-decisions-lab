12dd092138406cc8bbd816112d649959229adb8a
ehofer42 <elise.hofer42@gmail.com>
2021-11-16 07:05:00 -0700

    Problem 2 & 3 completed

--- lab.js
+++ lab.js
@@ -27,7 +27,12 @@ var brittanisBirthYear = 1986
 */
 
 //CODE HERE
-
+if (amysAge > brittanisAge){
+console.log("Amy is older")
+}else if (brittanisAge > amysAge){
+console.log("Brittani is older")
+} else {console.log("They are the same age")
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -36,7 +41,11 @@ var brittanisBirthYear = 1986
 */
 
 //CODE HERE
-
+if (amysBirthYear === brittanisBirthYear){
+console.log("Amy & Brittani were born in the same year")
+}else {
+console.log("Amy & Brittani were not born in the same year")
+}
 
 ////////// PROBLEM 4 //////////
 
